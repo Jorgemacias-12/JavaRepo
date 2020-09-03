d129aa37fa1808f02311e5588240c4239818ed80
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-03 12:29:16 -0500

    He añadido una nueva clase donde se expondra como hacer una SplashScreen.

---
+++ src/com/jamz/SplashScreen/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.jamz.SplashScreen;
+
+
+/** -------------------------------------------
+        @author: JAMZ    
+
+        File: SplashScreen.java
+        Date: 3/09/2020 
+        Time: 12:28:56 PM
+        
+        Purpose:
+
+   
+    -------------------------------------------*/  
+
+public class SplashScreen {
+
+}
