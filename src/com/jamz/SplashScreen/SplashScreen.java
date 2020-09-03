1dba4fced636a0d0acf9a7f4cf5d9e5a22e8c5a7
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-03 15:55:22 -0500

    Se ha terminado el código que nos permite un buen funcionamiento.

--- src/com/jamz/SplashScreen/SplashScreen.java
+++ src/com/jamz/SplashScreen/SplashScreen.java
@@ -1,5 +1,10 @@
 package com.jamz.SplashScreen;
 
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+import javax.swing.Timer;
+
 
 /** -------------------------------------------
         @author: JAMZ    
@@ -8,11 +13,33 @@ package com.jamz.SplashScreen;
         Date: 3/09/2020 
         Time: 12:28:56 PM
         
-        Purpose:
+        Purpose: Ejemplo de como iniciar una aplicación
+        * con una barra de progreso.
 
    
     -------------------------------------------*/  
 
-public class SplashScreen {
-
+public class SplashScreen extends JFrame{
+   
+    private static int tempCounter = 0;
+    private static Timer timer;
+    
+    public static void initApplication(JProgressBar progress, boolean paintProgress){
+        ActionListener ac = (ActionListener) ->{
+            if(paintProgress){
+               progress.setStringPainted(true);
+               progress.setString((tempCounter++)+"%");
+               progress.setValue(tempCounter++);
+               progress.setForeground(new java.awt.Color(6,176,37));
+            }else{
+               progress.setValue(tempCounter++);
+            }
+            if(progress.getValue()  == 100){
+                timer.stop();
+            }
+        };
+        timer = new Timer(100, ac);
+        timer.start();
+    }
+    
 }
