1e644bc88d917b8fa3dede1f25d3fe0f43c31eb2
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-03 10:32:28 -0500

    Clase para validar campos de texto, con prueba de forma gráfica.

---
+++ src/com/jamz/validations/TextField.java
@@ -0,0 +1,19 @@
+package com.jamz.validations;
+
+import javax.swing.JTextField;
+
+/** -------------------------------------------
+        @author: JAMZ    
+
+        File: TextField.java
+        Date: 3/09/2020 
+        Time: 10:18:41 AM
+        
+        Purpose:
+
+   
+    -------------------------------------------*/  
+
+public class TextField extends JTextField{
+
+}
