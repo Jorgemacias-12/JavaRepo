b2a4a9f3332a3f5b49ebb656633c586dc8a54b6a
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-04 09:20:49 -0500

    Se ha añadido un método que permite valiar una CURP.

--- src/com/jamz/regularExpresions/stringValidations.java
+++ src/com/jamz/regularExpresions/stringValidations.java
@@ -10,9 +10,14 @@ package com.jamz.regularExpresions;
         Purpose: validar la entrada del usuario
         * dependiendo del método requerido.
 
-   
     -------------------------------------------*/  
 
 public class stringValidations {
-    
+    /**
+     * @param input entrada de tipo String (texto).
+     * @return Valor verdadero o falso dependiendo de la entrada.
+     */
+    public static boolean ValidateCURP(String input) {
+        return input.matches("^[A-Z]{4}[\\d]{6}([H]|[M])(AS|BC|BS|CC|CL|CS|CM|CH|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[A-Z]{3}(\\w){2}$");
+    }
 }
