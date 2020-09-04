395871012b1abdfbb8d3629655a4d62ded4e8cc6
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-04 09:35:15 -0500

    Se ha añadido una nueva clase, su proposito es redimensionar imagenes.

---
+++ src/com/jamz/Images/reScale.java
@@ -0,0 +1,17 @@
+package com.jamz.Images;
+
+/** -------------------------------------------
+        @author: JAMZ    
+
+        File: reScale.java
+        Date: 4/09/2020 
+        Time: 09:34:59 AM
+        
+        Purpose: redimensionar imagenes.
+
+   
+    -------------------------------------------*/  
+
+public class reScale {
+
+}
