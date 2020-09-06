2c46a36b6be37e2e49aca75763a6482e94d1935b
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-06 13:17:58 -0500

    Se ha añadido una clase que nos permite mover un frame en estado undecorate.

---
+++ src/com/jamz/dragAndDropUndecorated/undecoratedWindow.java
@@ -0,0 +1,15 @@
+package com.jamz.dragAndDropUndecorated;
+
+/**
+    @author: Jorge Antonio Macias Zambrano
+    File: undecoratedWindow.java
+    Date: 6/09/2020
+    Time: 01:12:41 PM
+
+    Purpose:
+
+*/
+
+public class undecoratedWindow {
+    
+}
