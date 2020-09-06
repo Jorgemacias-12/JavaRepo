ea2e8a0cfbf8fa2236d3b6a81d5f1fab61ca0094
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-06 17:34:22 -0500

    Añadiendo clase que permitira dibujar en Paneles.

---
+++ src/com/jamz/graphics/drawManager.java
@@ -0,0 +1,15 @@
+package com.jamz.graphics;
+
+/**
+    @author: Jorge Antonio Macias Zambrano
+    File: drawManager.java
+    Date: 6/09/2020
+    Time: 05:33:59 PM
+
+    Purpose:
+
+*/
+
+public class drawManager {
+
+}
