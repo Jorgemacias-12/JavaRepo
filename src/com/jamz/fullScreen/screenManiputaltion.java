7cc16ff6e473f975872258107c6c327112a43e3d
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-06 14:13:50 -0500

    Clase para manipular el como el usuario ve una ventana.

---
+++ src/com/jamz/fullScreen/screenManiputaltion.java
@@ -0,0 +1,15 @@
+package com.jamz.fullScreen;
+
+/**
+    @author: Jorge Antonio Macias Zambrano
+    File: screenManiputaltion.java
+    Date: 6/09/2020
+    Time: 02:13:38 PM
+
+    Purpose:
+
+*/
+
+public class screenManiputaltion {
+
+}
