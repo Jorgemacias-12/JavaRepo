ebfb98c511943e040cc1d09524b12a216118912f
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-10-04 18:51:49 -0500

    New method documentation.

--- src/com/jamz/Images/reScale.java
+++ src/com/jamz/Images/reScale.java
@@ -30,16 +30,34 @@ public class reScale
     }
     //
     
+    /*
+        Public non static methods.
+    */
     
+    /**
+     * This function allow to resize an ImageIcon, using a file object how
+     * to input.
+     * @param file A file object with the route of the selected image.
+     * @param newWidth The new width of the image.
+     * @param newHeight The new height of the image.
+     * @return  Returns resized image.
+     */
     public ImageIcon scaleImageIcon(File file, int newWidth, int newHeight)
     {
-        try {
+        try 
+        {
             Image image = ImageIO.read(file);
             return new ImageIcon(image.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH));
-        } catch (IOException ex) {
+        } 
+        catch (IOException ex) 
+        {
             Logger.getLogger(reScale.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
+    
+    /*
+        Public static methods.
+    */
     
 }
