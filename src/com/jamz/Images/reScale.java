8b0aa663cd282acde4cc4590c334b48bb5a317d1
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-10-04 18:32:51 -0500

    funcion que escala imageIcon añadida.

--- src/com/jamz/Images/reScale.java
+++ src/com/jamz/Images/reScale.java
@@ -1,5 +1,13 @@
 package com.jamz.Images;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
 /**
  * This class can resize Images
  * 
@@ -12,22 +20,26 @@ public class reScale
      * Class variables.
      */
     
-    private int Width;
-    private int Height;
     
     /**
      * Classes constructors.
      */
     public reScale()
     {
-        Width = 0;
-        Height = 0;
+
     }
+    //
     
-    public reScale(int inputWidth, int inputHeight)
+    
+    public ImageIcon scaleImageIcon(File file, int newWidth, int newHeight)
     {
-        Width = inputWidth;
-        Height = inputHeight;
+        try {
+            Image image = ImageIO.read(file);
+            return new ImageIcon(image.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH));
+        } catch (IOException ex) {
+            Logger.getLogger(reScale.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
     
 }
